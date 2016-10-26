9773a9a894594bf322d6471a382646ed2333eb4f
Jason Mize <jasonemize@gmail.com>
2016-10-26 11:29:37 -0400

    answer list fully formed, shown on page, user selection sent to function userChoice

--- client/art/art-page.controller.js
+++ client/art/art-page.controller.js
@@ -16,8 +16,8 @@ function ArtPageController(artAPIService) {
     // use object for multiple choice
     function randomPic() {
         ctrl.maxRange = Math.floor(ctrl.paintings.length);
-        ctrl.randomArtworkIndex = Math.floor(Math.random() * (ctrl.maxRange));
-        ctrl.randomPainting = ctrl.paintings[ctrl.randomArtworkIndex];
+        ctrl.artIndex = Math.floor(Math.random() * (ctrl.maxRange));
+        ctrl.randomPainting = ctrl.paintings[ctrl.artIndex];
         return ctrl.randomPainting;
     }
 
@@ -34,8 +34,8 @@ function ArtPageController(artAPIService) {
     // get random objects from the Artist array
     function randomArtist() {
         ctrl.maxRange = Math.floor(ctrl.artists.length);
-        ctrl.randomArtistIndex = Math.floor(Math.random() * (ctrl.maxRange));
-        return ctrl.randomArtistIndex;
+        ctrl.artistIndex = Math.floor(Math.random() * (ctrl.maxRange));
+        return ctrl.artistIndex;
     }
 
 
@@ -46,13 +46,39 @@ function ArtPageController(artAPIService) {
         while (ctrl.numberOfWrongAnswers > 0) {
             randomArtist();
 
-            // don't add the real artist to the wrong answer list
-            if (ctrl.artists[ctrl.randomArtistIndex].name !== ctrl.randomPainting.artist.name) {
-                ctrl.wrongAnswers.push(ctrl.artists[ctrl.randomArtistIndex].name);
-                ctrl.numberOfWrongAnswers -= 1;
+            // add to wrong answer list as long as it isn't the right answer
+            if (ctrl.artists[ctrl.artistIndex].name !== ctrl.randomPainting.artist.name) {
+                // add to wrong answer list as long as not a duplicate
+                if (ctrl.wrongAnswers.indexOf(ctrl.artists[ctrl.artistIndex].name) === -1) {
+                    ctrl.wrongAnswers.push(ctrl.artists[ctrl.artistIndex].name);
+                    ctrl.numberOfWrongAnswers -= 1;
+                }
             }
         }
-    }
+        // add correct answer into wrong answers
+        ctrl.wrongAnswers.push(ctrl.randomPainting.artist.name);
+        // shuffle list
+        ctrl.answerList = [];
+        while (ctrl.wrongAnswers.length > 0) {
+            // pull random name from wrong answers
+            ctrl.maxRange = Math.floor(ctrl.wrongAnswers.length);
+            ctrl.answerIndex = Math.floor(Math.random() * (ctrl.maxRange));
+            ctrl.name = ctrl.wrongAnswers.splice(ctrl.answerIndex, 1);
+            // place name in answer list
+            // strip off quotes and brackets
+            for (let i = 0; i < ctrl.name.length; i += 1) {
+                ctrl.answerList.push(ctrl.name[i]);
+            }
+        }
+    } // end wrongAnswers
+
+
+    // take user choice and determine right/wrong and take appropriate action
+    ctrl.userChoice = function userChoice(selection) {
+        console.log('user answer: ', selection);
+        
+    };
+
 
     getArt().then(randomPic).then(getArtist).then(wrongAnswers);
 }

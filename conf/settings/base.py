7c1306628b25199337ffdc4d53dfef4256d567f8
Jason Mize <jasonemize@gmail.com>
2018-10-09 19:01:26 -0400

    config

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -17,7 +17,8 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '1234569'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = []
